ed2af54ecd649ee4c3a82f14dcfba9f6a1002030
SaintPeter <rex.schrader@gmail.com>
2020-09-01 09:19:25 -0700

    Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.setOptions({
+  breaks: true
+})
+
+let defaultMD = `# Header 1
+## Header 2
+
+Link Example: [This is a Link!](http://freecodecamp.org)
+
+Just some sameple text
+So sampley!
+Look at them linebreaks!
+
+Inline Code: \`Inline Code\` Is here!
+
+\`\`\`
+Code Block here!
+\`\`\`
+
+* List Item 1
+* List Item 2
+* List Item 3
+
+> Blockquote Example
+
+**Bold Text**  
+
+Image:
+![](https://picsum.photos/200/300)
+`;
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      content: defaultMD,
+      markdown_content: marked(defaultMD)
+    }
+    
+    this.textarea_keyup = this.textarea_keyup.bind(this)
+    this.getMarkdownText = this.getMarkdownText.bind(this)
+  }
+  
+  textarea_keyup(e) {
+    this.setState({
+        content: e.target.value,
+        markdown_content: marked(e.target.value) 
+    });
+  }
+  
+  getMarkdownText() {
+    return { __html: this.state.markdown_content}
+  }
+  
+  render() {
+    return (
+        [ <h1 style={{"textAlign": "center"}}>Free Code Camp - Markdown Previewer Project</h1>,
+        <div id="container">          
+          <div className="column">
+            <h2>Markdown</h2>
+            <textarea id="editor" onChange={this.textarea_keyup} value={this.state.content} />
+          </div>
+
+        <div className="column">
+          <h2>Preview</h2>
+          <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()} />
+        </div>
+      </div> ]
+    );
+  }
+  
 }
 
 export default App;
